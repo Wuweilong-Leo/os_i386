7fb986f6ab8399a8001a264b0a8c462bca580676
wwl <790754967@qq.com>
2023-06-08 21:46:37 +0800

    改进中断

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -69,8 +69,6 @@ static void general_intr_handler(uint8_t vec_nr)
     put_str("int vector : 0x");
     put_int(vec_nr);
     put_char('\n');
-    put_str(intr_name[vec_nr]);
-    put_char('\n');
 }
 
 const char *intr_str[] = {
